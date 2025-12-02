4656355fd36e6603c425678eb649bf8e1881dbe5
NavyGhost33 <tb12best@gmail.com>
2025-12-02 07:38:17 +0200

    Fix "document.write" in "alert".

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,28 +1,25 @@
 
 // 1
 
-let sideSquare = prompt("What is the length of the side of the square (sm)?");
+let sideSquare = prompt("Enter the length of the side of the square (sm.)");
 console.log(sideSquare);
-
-// document.write("Hello" + sideSquare + "!");
-// alert(sideSquare);
 
 let perimeter = sideSquare * 4;
 console.log(perimeter);
-document.write("Perimeter of a square = " + perimeter + " sm.");
+alert("Perimeter of a square = " + perimeter + " sm.");
 
 // 2
 
-let cubeLength = prompt("Indicate the length of the edge of the cube (cm.)");
+let cubeLength = prompt("Enter the length of the edge of the cube (cm.)");
 console.log(cubeLength);
 
 let cubeVolume = cubeLength * cubeLength * cubeLength;
 console.log(cubeVolume);
-document.write("Cube volume = " + cubeVolume + " cm.");
+alert("Cube volume = " + cubeVolume + " cm.");
 
 let faceArea = cubeLength * cubeLength;
 console.log(faceArea);
-document.write("Cube face area = " + faceArea + " cm.");
+alert("Cube face area = " + faceArea + " cm.");
 
 // 3
 
@@ -31,11 +28,11 @@ console.log(circleDiameter);
 
 let circumference = Math.PI * circleDiameter;
 console.log(circumference.toFixed(2));
-document.write("Circumference = " + circumference.toFixed(2) + " cm. ");
+alert("Circumference = " + circumference.toFixed(2) + " cm. ");
 
 let areaCircle = Math.PI * ((circleDiameter / 2) ** 2);
 console.log(areaCircle.toFixed(2));
-document.write("Area of a circle = " + areaCircle.toFixed(2) + " cm.");
+alert("Area of a circle = " + areaCircle.toFixed(2) + " cm.");
 
 // 4
 
@@ -47,7 +44,7 @@ console.log(volume.toFixed(2));
 
 let density = mass / volume;
 console.log(density.toFixed(2));
-document.write("Density of the substance - " + density.toFixed(2) + " gm/cm³.")
+alert("Density of the substance - " + density.toFixed(2) + " gm/cm³.")
 
 // 5
 
@@ -59,7 +56,7 @@ console.log(area.toFixed(2));
 
 let populationDensity = population / area; 
 console.log(populationDensity.toFixed(2));
-document.write("Population density - " + populationDensity.toFixed(2) + " people per 1 km².");
+alert("Population density - " + populationDensity.toFixed(2) + " people per 1 km².");
 
 // 6
 
@@ -71,7 +68,7 @@ console.log(legB);
 
 let hypotenuse = Math.sqrt(legA ** 2 + legB **2);
 console.log(hypotenuse.toFixed(2));
-document.write("Length of the hypotenuse - " + hypotenuse .toFixed(2)+ " cm.");
+alert("Length of the hypotenuse - " + hypotenuse .toFixed(2)+ " cm.");
 
 // 7
 
@@ -80,5 +77,5 @@ console.log(a);
 
 let y = a ** 2 + 10 / Math.sqrt(a ** 2 + 1);
 console.log(y.toFixed(2));
-document.write("y value - " + y.toFixed(2) + ".");
+alert("y value - " + y.toFixed(2) + ".");
 
